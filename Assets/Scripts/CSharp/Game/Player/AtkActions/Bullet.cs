c73eff9a5301dfb4b7dc84f744eac400bfffa4ae
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2020-08-31 21:48:29 -0400

    Updated to 2019.4.9f1 and added Bullet class
    Updated all data to 2019.4.9f1
    Added Bullet class

---
+++ Assets/Scripts/CSharp/Game/Player/AtkActions/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace kobk.csharp.game.player.attack
+{
+    public class Bullet : NetworkBehaviour
+    {
+        
+    }
+}
